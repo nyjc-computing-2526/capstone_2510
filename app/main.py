43cb8972db6d472b34c83aeb9a2172a230d5fd0f
sparsetable <francis.ang.zhao.xiong_2025@nyjc.edu.sg>
2026-05-04 06:06:38 +0000

    Working app

--- app/main.py
+++ app/main.py
@@ -4,8 +4,9 @@ Run the app.
 
 from app_obj import app
 
-# These imports mutate app
+# These imports mutate the app object
 import routes.activities
+import routes.authentication
 import routes.landing
 
 app.run()
