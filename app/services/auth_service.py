bc3be32ad44810ae4fd61b118b93e71be43018b4
sparsetable <francis.ang.zhao.xiong_2025@nyjc.edu.sg>
2026-05-08 05:45:47 +0000

    Password hashing etc.

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -2,6 +2,8 @@
 Functions for managing users.
 To run properly, go into the app folder and do
     python3 -m services.auth_service
+    or, within the app folder
+    import services.auth_service
 
 User schema:
 - id: int, internal DB id
@@ -12,6 +14,7 @@ User schema:
 """
 
 from db import db_execute
+import bcrypt
 
 def init_db():
     """
@@ -33,16 +36,35 @@ if __name__ == "__main__":
     if not prompt:
         init_db()
 
+def pw_hash(password: str) -> str:
+    """
+    Hash the given password with bcrypt.
+    Uses a random salt
+    """
+    data = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(data, salt)
+    return str(hashed, encoding = "ascii")
+
+def check_pw(hashed: str, attempt_plain: str) -> bool:
+    """
+    Pass in a password hash and the plaintext attempt.
+    Returns True if matches, False otherwise.
+    """
+    data = attempt_plain.encode("utf-8")
+    return bcrypt.checkpw(data, hashed.encode("ascii"))
+
 def create_user(name: str, email: str, password: str):
     """
     Creates a new user.
+    Password is plaintext in this context.
     """
     db_execute(
         "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)",
-        (name, email, password)
+        (name, email, pw_hash(password))
     )
 
-def email_exists(email):
+def email_exists(email: str):
     """
     Check if the provided email has a registered account.
     """
@@ -53,14 +75,15 @@ def email_exists(email):
 
     return len(result) > 0
 
-def get_user(email):
+def get_user(email: str) -> list:
     """
     Fetches user info for provided email.
     Make sure that the email is actually registered.
+    returns [id, name, email, passsword]
     """
     _, result = db_execute(
         "SELECT * FROM users WHERE email = %s",
         (email,)
     )
 
-    return result[0]
+    return list(result[0])
